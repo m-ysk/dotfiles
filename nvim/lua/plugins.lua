194bb6d70f7d5d18f6e6b51c17a8be4d40902922
m-ysk <myskjp@gmail.com>
2022-02-10 16:02:34 +0900

    Introduce telescope.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -28,13 +28,20 @@ require('packer').startup(function()
 
 	use {'jose-elias-alvarez/null-ls.nvim', requires = {'nvim-lua/plenary.nvim'}}
 
+	use 'sheerun/vim-polyglot'
+
 	use 'rust-lang/rust.vim'
 	vim.g.rustfmt_autosave = 1
+
+	--use 'simrat39/rust-tools.nvim'
 
 	use 'HerringtonDarkholme/yats.vim'
 	use 'maxmellon/vim-jsx-pretty'
 	use 'styled-components/vim-styled-components'
 	use 'jose-elias-alvarez/nvim-lsp-ts-utils'
+
+	use 'tpope/vim-repeat'
+	use 'tpope/vim-surround'
 
 	use 'ntpeters/vim-better-whitespace'
 	use 'frazrepo/vim-rainbow'
@@ -43,49 +50,76 @@ require('packer').startup(function()
 	use 'Yggdroot/indentLine'
 
 	use 'vim-airline/vim-airline'
-	use 'itchyny/lightline.vim'
+	--use 'itchyny/lightline.vim'
 
-	use 'scrooloose/nerdtree'
-	api.nvim_set_keymap('', '<Leader>e', ':NERDTreeToggle<CR>', {})
-	vim.g.NERDTreeShowHidden = 1
+	--use 'scrooloose/nerdtree'
+	--api.nvim_set_keymap('', '<Leader>e', ':NERDTreeToggle<CR>', {})
+	--vim.g.NERDTreeShowHidden = 1
+
+	use 'lambdalisue/fern.vim'
+	api.nvim_set_keymap('', '<Leader>e', ':Fern . -reveal=% -drawer -toggle -width=40<CR>', opts)
 
 	use 'xuyuanp/nerdtree-git-plugin'
 	use 'airblade/vim-gitgutter'
 	use 'tpope/vim-fugitive'
-	api.nvim_set_keymap('n', '<Leader>gs', ':Git<CR>', opts)
-	api.nvim_set_keymap('n', '<Leader>gc', ':Gcommit-v<CR>', opts)
-	api.nvim_set_keymap('n', '<Leader>ga', ':Gwrite<CR>', opts)
-	api.nvim_set_keymap('n', '<Leader>gr', ':Gread<CR>', opts)
-	api.nvim_set_keymap('n', '<Leader>gl', ':Glog<CR>', opts)
-	api.nvim_set_keymap('n', '<Leader>gb', ':Git blame<CR>', opts)
+	--api.nvim_set_keymap('n', '<Leader>gs', ':Git<CR>', opts)
+	--api.nvim_set_keymap('n', '<Leader>gc', ':Gcommit-v<CR>', opts)
+	--api.nvim_set_keymap('n', '<Leader>ga', ':Gwrite<CR>', opts)
+	--api.nvim_set_keymap('n', '<Leader>gr', ':Greafad<CR>', opts)
+	--api.nvim_set_keymap('n', '<Leader>gl', ':Gclog<CR>', opts)
+	api.nvim_set_keymap('n', '<Leader>gbl', ':Git blame<CR>', opts)
 	api.nvim_set_keymap('n', '<Leader>gd', ':Gdiff<CR>', opts)
 
-	use 'junegunn/fzf'
-	use 'junegunn/fzf.vim'
-	api.nvim_set_keymap('', '<Leader>ff', ':Files<CR>', {})
-	api.nvim_set_keymap('', '<Leader>gg', ':Rg<CR>', {})
-	vim.cmd([[
-	  command! -bang -nargs=* Rg
-		\ call fzf#vim#grep(
-		\   'rg --line-number --no-heading '.shellescape(<q-args>), 0,
-		\   fzf#vim#with_preview({'options': '--exact --reverse'}, 'right:50%:wrap'))
-	]])
+	--use 'junegunn/fzf'
+	--use 'junegunn/fzf.vim'
+	--api.nvim_set_keymap('', '<Leader>ff', ':Files<CR>', {})
+	--api.nvim_set_keymap('', '<Leader>gg', ':Rg<CR>', {})
+	--vim.cmd([[
+	--  command! -bang -nargs=* Rg
+	--	\ call fzf#vim#grep(
+	--	\   'rg --line-number --no-heading '.shellescape(<q-args>), 0,
+	--	\   fzf#vim#with_preview({'options': '--exact --reverse'}, 'right:50%:wrap'))
+	--]])
+
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	api.nvim_set_keymap('n', '<Leader>ff', "<cmd>lua require('telescope.builtin').find_files()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>fg', "<cmd>lua require('telescope.builtin').git_files()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>gg', "<cmd>lua require('telescope.builtin').live_grep()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>fb', "<cmd>lua require('telescope.builtin').buffers()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>d', "<cmd>lua require('telescope.builtin').lsp_definitions()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>t', "<cmd>lua require('telescope.builtin').lsp_type_definitions()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>r', "<cmd>lua require('telescope.builtin').lsp_references()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>i', "<cmd>lua require('telescope.builtin').lsp_implementations()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>fsb', "<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>fsa', "<cmd>lua require('telescope.builtin').lsp_workspace_symbols()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>fc', "<cmd>lua require('telescope.builtin').lsp_code_actions()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>fd', "<cmd>lua require('telescope.builtin').diagnostics()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>ft', "<cmd>lua require('telescope.builtin').treesitter()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>gla', "<cmd>lua require('telescope.builtin').git_commits()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>glb', "<cmd>lua require('telescope.builtin').git_bcommits()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>gbr', "<cmd>lua require('telescope.builtin').git_branches()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>gs', "<cmd>lua require('telescope.builtin').git_status()<CR>", opts)
 
 	use {
 		'phaazon/hop.nvim',
 		branch = 'v1',
 	}
 	require'hop'.setup()
-	vim.api.nvim_set_keymap('n', '<Leader>s', "<cmd>lua require'hop'.hint_char1({ case_insensitive = false })<CR>", {})
-	vim.api.nvim_set_keymap('n', '<Leader>a', "<cmd>lua require'hop'.hint_words()<CR>", {})
-	vim.api.nvim_set_keymap('n', '<Leader>z', "<cmd>lua require'hop'.hint_lines_skip_whitespace()<CR>", {})
+	api.nvim_set_keymap('n', 'f', "<cmd>lua require'hop'.hint_char1({ case_insensitive = true })<CR>", opts)
+	api.nvim_set_keymap('o', 'f', "<cmd>lua require'hop'.hint_char1({ case_insensitive = true, inclusive_jump = true })<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>s', "<cmd>lua require'hop'.hint_words()<CR>", opts)
+	api.nvim_set_keymap('n', '<Leader>a', "<cmd>lua require'hop'.hint_lines_skip_whitespace()<CR>", opts)
 
 	if packer_bootstrap then
 		require('packer').sync()
 	end
 end)
 
-vim.cmd([[autocmd BufWritePost init.lua source <afile> | PackerCompile]])
+vim.cmd([[autocmd BufWritePost init.lua source afile | PackerCompile]])
 
 require('nvim-treesitter.configs').setup {
 	highlight = {
@@ -104,9 +138,9 @@ lsp_installer.on_server_ready(function(server)
 		local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 
 		local opt = { noremap = true, silent = true }
-		buf_set_keymap('n', '<Leader>d', '<cmd>lua vim.lsp.buf.definition()<CR>', opt)
-		buf_set_keymap('n', '<Leader>i', '<cmd>lua vim.lsp.buf.implementation()<CR>', opt)
-		buf_set_keymap('n', '<Leader>r', '<cmd>lua vim.lsp.buf.references()<CR>', opt)
+		--buf_set_keymap('n', '<Leader>d', '<cmd>lua vim.lsp.buf.definition()<CR>', opt)
+		--buf_set_keymap('n', '<Leader>i', '<cmd>lua vim.lsp.buf.implementation()<CR>', opt)
+		--buf_set_keymap('n', '<Leader>r', '<cmd>lua vim.lsp.buf.references()<CR>', opt)
 		buf_set_keymap('n', '<Leader><C-k>', '<cmd>lua vim.lsp.buf.hover()<CR>', opt)
 		buf_set_keymap('n', '<Leader>fm', '<cmd>lua vim.lsp.buf.formatting_sync()<CR>', opt)
 	end
