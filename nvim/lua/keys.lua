f65e34eac31eb3771dbca8bae40dc8b70fe5da4b
Yoshiki Matsuda <myskjp@gmail.com>
2022-10-03 19:16:52 +0900

    update installation script for packer

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -7,7 +7,7 @@ local opt = { noremap = true, silent = false }
 
 api.nvim_set_keymap('i', 'jj', '<Esc>', opt)
 api.nvim_set_keymap('i', '<C-b>', '<C-o>h', opt)
-api.nvim_set_keymap('i', '<C-f>', '<C-o>l', opt)
+api.nvim_set_keymap('i', '<C-f>', '<C-o>a', opt)
 api.nvim_set_keymap('i', '<C-a>', '<C-o>^', opt)
 api.nvim_set_keymap('i', '<C-e>', '<C-o>$', opt)
 api.nvim_set_keymap('i', '<C-k>', '<C-o>d$', opt)
