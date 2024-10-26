61c01ea35ca11df4f3cc590eb32ceb0c675eb500
Yoshiki Matsuda <myskjp@gmail.com>
2024-10-26 15:19:35 +0900

    Fix keybind for nvim-cmp

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -244,7 +244,7 @@ vim.opt.completeopt = 'menu,menuone,noselect'
 local cmp = require'cmp'
 cmp.setup({
 	mapping = {
-		['<C-d>'] = cmp.mapping.scroll_docs(-4),
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.close(),
